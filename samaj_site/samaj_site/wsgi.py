0e0e3f98cadaeef53f32acbfc275b2972a0d40de
JiniyasSuthr-2001 <jiniyassuthar87@gmail.com>
2025-12-20 13:26:48 +0530

    Prepare for PythonAnywhere deployment

--- samaj_site/samaj_site/wsgi.py
+++ samaj_site/samaj_site/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# add your project directory to the sys.path
+project_home = '/home/Kantha27MevadaSuthar/samaj_site'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'samaj_site.settings'
+
+# activate virtualenv
+activate_this = '/home/Kantha27MevadaSuthar/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samaj_site.settings')
-
 application = get_wsgi_application()
