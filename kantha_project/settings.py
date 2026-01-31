2b720b6ad6e634ab95a4c884908aec4c32eba7e8
JiniyasSuthr-2001 <jiniyassuthar87@gmail.com>
2026-01-31 15:33:50 +0530

    Allow PythonAnywhere host

--- kantha_project/settings.py
+++ kantha_project/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-reconstruction-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shreekantha27mevadasuthar.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
